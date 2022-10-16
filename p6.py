8d4f50d8902d179bb4f738e140f6483ef5767462
Sarp Eren EGILMEZ <2348.sarp.egilmez.2006@gmail.com>
2022-10-16 09:35:16 +0300

    Create p6.py

---
+++ p6.py
@@ -0,0 +1,9 @@
+from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
+from spike.control import wait_for_seconds, wait_until, Timer
+from math import *
+
+hub = PrimeHub()
+
+for i in range(6):
+    hub.light_matrix.write(str(i))
+    wait_for_seconds(0.5)
