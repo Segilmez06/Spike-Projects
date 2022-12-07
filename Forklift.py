021d64d1a33b6857473f3a8f8d2b84a80ccb8a1f
Sarp Eren EGILMEZ <2348.sarp.egilmez.2006@gmail.com>
2022-12-07 19:10:52 +0300

    Update Forklift.py

--- Forklift.py
+++ Forklift.py
@@ -2,33 +2,44 @@ from spike import *
 from spike.control import *
 from math import *
 
+platform_algilama_mesafesi = 6
+hareket_hizi = 10
+kaldirma_hizi = 10
+
 hub = PrimeHub()
 
 motors = MotorPair('E', 'A')
 lift_motor = Motor('C')
 sensor = DistanceSensor('B')
+color = ColorSensor('D')
 
-motors.set_default_speed(10)
-motors.set_stop_action('coast')
-lift_motor.set_default_speed(15)
+# motors.set_default_speed(hareket_hizi)
+motors.set_stop_action('brake')
+lift_motor.set_default_speed(kaldirma_hizi)
+lift_motor.set_stop_action('coast')
 
-motors.start()
-sensor.wait_for_distance_closer_than(12)
+motors.start(0, 15)
+
+sensor.light_up_all(100)
+while color.get_reflected_light() < 95: pass
+sensor.light_up_all(0)
+
 motors.stop()
-lift_motor.run_for_degrees(-520)
-motors.move(.35, 'rotations', 0, 10)
-lift_motor.run_for_degrees(520)
-# sensor.wait_for_distance_closer_than(6)
-# motors.stop()
+lift_motor.run_for_degrees(-500)
+motors.move(.9, 'rotations', 0, 10)
+lift_motor.run_for_degrees(500)
+# lift_motor.start()
+# wait_for_seconds()
 
-# while True:
-    # hub.left_button.wait_until_pressed()
-    # lift_motor.run_for_degrees(-2)
-    # hub.left_button.wait_until_pressed()
-    # lift_motor.run_for_degrees(1)
-    # hub.left_button.wait_until_pressed()
-    # lift_motor.run_for_degrees(520)
-    # wait_for_seconds(.5)
-    # hub.left_button.wait_until_pressed()
-    # lift_motor.run_for_degrees(-25)
-    # wait_for_seconds(.5)
+motors.move(.66, 'rotations', 100, 10)
+
+motors.start(0, 10)
+sensor.wait_for_distance_closer_than(platform_algilama_mesafesi)
+lift_motor.start()
+motors.stop()
+
+lift_motor.stop()
+lift_motor.run_for_degrees(-75)
+motors.move(5, 'cm', 0, -10)
+motors.move(30, 'cm', 0, -100)
+lift_motor.run_for_degrees(80)
